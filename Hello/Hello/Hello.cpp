117fb03857b3d8be0a93781c2086de8b2e793f43
Jeff Haas <jeffhaas@unl.edu>
2016-09-27 15:26:14 -0500

    Initial in/out

---
+++ Hello/Hello/Hello.cpp
@@ -0,0 +1,17 @@
+// Hello.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	string s;
+	cout << "Hello, World\n\nEnter anything to exit> ";
+	cin >> s;
+    return 0;
+}
+

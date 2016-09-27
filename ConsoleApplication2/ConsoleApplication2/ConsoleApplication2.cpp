2828cfb12878207399aac51cb9b41f2675168562
Jeff Haas <jeffhaas@unl.edu>
2016-09-27 15:21:10 -0500

    gitignore changes

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string s;
+	std::cout << "Hello, World!\n";
+	std::cout << "\nType anything to exit > ";
+	std::cin >> s;
+    return 0;
+}
+
